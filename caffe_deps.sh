04d9265a6a4396c896eef60c9f047afba296f46a
lone-wulf <phani.geospoc@gmail.com>
2017-02-20 11:47:21 +0530

    caffe dependencies on ubuntu

---
+++ caffe_deps.sh
@@ -0,0 +1,8 @@
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler -y
+sudo apt-get install --no-install-recommends libboost-all-dev -y
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install the python-dev -y
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev -y
+sudo pip install protobuf 
+
+
